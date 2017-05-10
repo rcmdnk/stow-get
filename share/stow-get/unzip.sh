54bb0f0823c96366979a63331772f7cf7b4165e1
rcmdnk <rcmdnk@gmail.com>
2017-05-11 02:46:06 +0900

    fixed unzip

--- share/stow-get/unzip.sh
+++ share/stow-get/unzip.sh
@@ -1,7 +1,7 @@
 inst_type=tarball
 version=6.0
 url_prefix=ftp://ftp.info-zip.org/pub/infozip/src
-directory=${package}${version}
+directory=${package}${version/./}
 tarball=${directory}.tgz
 configure=""
 function make_cmd {
