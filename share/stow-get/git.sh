18d91f71581e9270eb575ee11d586e209ee83fa3
rcmdnk <rcmdnk@gmail.com>
2017-05-10 23:49:59 +0900

    fixed git version

--- share/stow-get/git.sh
+++ share/stow-get/git.sh
@@ -1,5 +1,7 @@
-version=2.13
+version=2.13.0
 inst_type=github
-before_configure="make configure"
+function before_configure {
+  make configure
+}
 bin_dep=(autoconf autoconf)
 lib_dep=(libcurl curl libexpat expat)
