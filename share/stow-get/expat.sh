c9416dd005e5864b58c6dd460e886c973b5f4bc6
rcmdnk <rcmdnk@gmail.com>
2017-06-18 06:13:43 +0900

    sete path first

--- share/stow-get/expat.sh
+++ share/stow-get/expat.sh
@@ -2,7 +2,11 @@ inst_type=tarball
 function get_latest {
   local output_detail="${1:-0}"
   local params="$(curl -k https://libexpat.github.io/ 2>/dev/null|grep -v Changelog|grep "Changes" -B1|head -n2)"
-  version="$(echo "$params"|tail -n1|cut -d ">" -f 2|cut -d "<" -f 1|cut -d " "  -f 2)"
+  version="$(echo "$params"|tail -n1|cut -d "/" -f 7|sed 's/^R_//'|sed 's/_/./g')"
+  if [ "$version" = "" ];then
+    err "Failed to get version for $package."
+    exit $EXIT_NO_VERSION
+  fi
   if [ "$output_detail" -eq 1 ];then
     local d="$(echo "$params"|cut -d "," -f 2)"
     local d="released on $(echo "$params"|head -n1|cut -d ">" -f2|cut -d " " -f1)"
