f7b1f329c1a1fe96cce22fbffaaef37beefad73e
rcmdnk <rcmdnk@gmail.com>
2017-07-18 17:30:29 +0900

    updated pkg-config

--- share/stow-get/pkg-config.sh
+++ share/stow-get/pkg-config.sh
@@ -1,2 +1,11 @@
 inst_type=tarball
 url_prefix=https://pkg-config.freedesktop.org/releases/
+glib_check=0
+if ! check_lib glib;then
+  lib_dep=(glib)
+else
+  glibv=($(echo $(glib-compile-schemas --version) 2>/dev/null|sed 's/\./ /g'))
+  if [ -z "${glibv}" ] || [ "${glib[0]}" -le 2 ] || [ "${glib[1]}" -le 15 ];then
+    lib_dep=(glib_install_package_glib)
+  fi
+fi
