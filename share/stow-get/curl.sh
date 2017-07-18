e49531a658f136ee8968a070e6c4c6737c0443b9
rcmdnk <rcmdnk@gmail.com>
2017-07-18 12:21:00 +0900

    added libtool, pkg-config, updaetd stow-get for get_latest_index

--- share/stow-get/curl.sh
+++ share/stow-get/curl.sh
@@ -1,7 +1,6 @@
 inst_type=tarball
 version_safe=7.54.1
 url_prefix="https://curl.haxx.se/download"
-index_for_latest="$url_prefix/"
 ssl_dir=$(check_lib ssl)
 if [ -z "$ssl_dir" ];then
   lib_dep=(ssl_package_openssl)
---
+++ share/stow-get/pkg-config.sh
@@ -0,0 +1,2 @@
+inst_type=tarball
+url_prefix=https://pkg-config.freedesktop.org/releases/
