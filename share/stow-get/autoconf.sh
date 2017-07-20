4db032b91b3934c867aa551794bd6daad309ffe0
rcmdnk <rcmdnk@gmail.com>
2017-07-20 18:39:41 +0900

    share/stow-get/autoconf.sh, from darwin16

--- share/stow-get/autoconf.sh
+++ share/stow-get/autoconf.sh
@@ -2,7 +2,7 @@ inst_type=gnu
 if check_bin m4;then
   m4_version=($(m4 --version|head -n1|cut -d' ' -f 4|tr . ' '))
   if [ "${m4_version[0]}" -lt 1 ] || ( [ "${m4_version[0]}" -eq 1 ] && [ "${m4_version[1]}" -lt 16 ] );then
-    bin_dep=(m4_install_package_autoconf)
+    bin_dep=(m4_install_package_m4)
   fi
 else
   bin_dep=(m4)
