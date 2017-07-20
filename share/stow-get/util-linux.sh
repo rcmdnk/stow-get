1edfd54e2b3a2fc58a102a999f76d1e7965fbb8d
rcmdnk <rcmdnk@gmail.com>
2017-07-20 19:02:12 +0900

    updated python util-linux

--- share/stow-get/util-linux.sh
+++ share/stow-get/util-linux.sh
@@ -1,6 +1,14 @@
 inst_type=github
 user=karelzak
 bin_dep=(autopoint_package_gettext)
+for p_v in "" 2.7 2.6 2.5 2.4;do
+  python_lib=$(check_lib libpython${p_v} 1)
+  if [ $? -eq 0 ];then
+    if [ ${python_lib#libpython${p_v}.} = "a" ];then
+      lib_dep=(python_install_package_python)
+    fi
+  fi
+done
 function before_configure {
   ./autogen.sh
 }
