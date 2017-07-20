45310891ca7463af8a73c169bfa7dfaa2e05f241
rcmdnk <rcmdnk@gmail.com>
2017-07-20 18:24:23 +0900

    updaetd libffi

--- share/stow-get/libffi.sh
+++ share/stow-get/libffi.sh
@@ -1,5 +1,13 @@
 inst_type=github
-bin_dep=(autoconf automake m4 libtool makeinfo_package_texinfo)
+bin_dep=(automake m4 libtool makeinfo_package_texinfo)
+if check_bin autoconf;then
+  autoconf_version=($(autoconf --version|head -n1|cut -d' ' -f 4|tr . ' '))
+  if [ "${autoconf_version[0]}" -lt 2 ] || ( [ "${autoconf_version[0]}" -eq 2 ] && [ "${autoconf_version[1]}" -lt 68 ] );then
+    bin_dep=(autoconf_install_package_autoconf ${bin_dep[@]})
+  fi
+else
+  bin_dep=(autoconf ${bin_dep[@]})
+fi
 function before_configure {
   ./autogen.sh
 }
