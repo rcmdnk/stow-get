5aa6c67200937970fc76fd9fb891557db951ac7b
rcmdnk <rcmdnk@gmail.com>
2017-07-21 19:14:49 +0900

    emacs update

--- share/stow-get/emacs.sh
+++ share/stow-get/emacs.sh
@@ -1,8 +1,12 @@
 inst_type=gnu
+configure_options="$configure_options --disable-ns-self-contained"
 xml2=$(xml2-config --cflags 2>/dev/null)
 if [ -z "$xml2" ];then
   configure_options="$configure_options --without-xml2"
 else
   configure_flags="CFLAGS=$(xml2-config --cflags)"
 fi
-configure_options="$configure_options --disable-ns-self-contained"
+gif=$(check_lib libgif 1)
+if [ -z "$gif" ];then
+  configure_options="$configure_options --without-xpm --without-gif"
+fi
